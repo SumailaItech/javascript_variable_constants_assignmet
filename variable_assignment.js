35c3a571ab4a4d906f93001768b3eec6493dc571
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-24 01:46:09 +0200

    Done with the assignment

--- variable_assignment.js
+++ variable_assignment.js
@@ -1,4 +1,15 @@
+
 let userInput = 10;
 let result;
+
 result = 18 + userInput;
 
+result = result - userInput;
+
+result = result * userInput;
+
+result = result / userInput;
+
+alert("Result of the opearations "+ result);
+
+alert("User Input "+ userInput);
